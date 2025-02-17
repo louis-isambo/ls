64510fcd428f32c4cd63c213a806c2c34eb2de08
louis-isambo <leisisambo@gmail.com>
2025-02-17 08:33:22 +0100

    update

--- js/leis.js
+++ js/leis.js
@@ -3,4 +3,9 @@
 if(window.LDA){
     pageApi.invoke("fls:add", null, window.LDA.LSS)
     pageApi.invoke("fls:add", null, window.LDA.LSA)
+
+    window.LDA.isServer(function(){
+        console.log("server running....");
+        
+    })
 }
